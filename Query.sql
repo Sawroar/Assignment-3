3b61229d5c88fa86ab2926f14fe041c42f9c9128
Sawroare <sawroarehossain5@gmail.com>
2026-06-16 17:53:00 +0600

    user inserted into users  table Created

--- Query.sql
+++ Query.sql
@@ -17,3 +17,24 @@ CREATE TABLE Matches(
     base_ticket_price NUMERIC(10, 2) CHECK(base_ticket_price >= 0),
     match_status VARCHAR(50) CHECK(match_status IN('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
 );
+
+
+CREATE TABLE Bookings(
+    booking_id INT PRIMARY KEY,
+    user_id INT,
+    match_id INT,
+    seat_number VARCHAR(20),
+    payment_status VARCHAR(50) CHECK(payment_status IN('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
+    total_cost NUMERIC(10, 2) CHECK(total_cost >= 0),
+
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (match_id) REFERENCES Matches(match_id)
+);
+
+
+
+INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
